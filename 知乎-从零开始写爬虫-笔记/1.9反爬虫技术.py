451ab1e7ae943fdb6f1bd69b656c296a23869801
gentiana <gentiana@163.com>
2017-11-15 11:25:20 +0800

    2017年11月15日11:25:13

--- 知乎-从零开始写爬虫-笔记/1.9反爬虫技术.py
+++ 知乎-从零开始写爬虫-笔记/1.9反爬虫技术.py
@@ -5,11 +5,32 @@
 
 __author__ = 'Gentiana'
 
-import requestshttp://113.230.76.170:5333/WebReport/ReportServer?reportlet=zsfxck/20w/20wjs.cpt&op=write&stime=&endtime=&gh=system&userid=40288d9e58425bc8015842649c580005&username=%E
+import requests
 from bs4 import BeautifulSoup
 import random
 
 # 从一系列的user-agent里随机挑出一个符合标准的使用
-agents = []
-fakeheader = {}
-fakeheader['User-agent'] = agents[random.randint(0, len(agents))]
+def get_agent():
+    '''
+    模拟header的user-agent字段，
+    返回一个随机的user-agent字典类型的键值对
+    '''
+    agents = ['Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0;',
+              'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv,2.0.1) Gecko/20100101 Firefox/4.0.1',
+              'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11',
+              'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
+              'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)']
+    fakeheader = {}
+    fakeheader['User-agent'] = agents[random.randint(0, len(agents))]
+    return fakeheader
+
+    # 注意看新的请求函数：
+
+    def get_html(url):
+        try:
+            r = requests.get(url, timeout=30, headers=get_agent())
+            r.raise_for_status
+            r.encoding = r.apparent_encoding
+            return r.status_code
+        except:
+            return "Someting Wrong！"
